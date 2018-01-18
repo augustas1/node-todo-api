db259f27714d1d8ac3679be01cc8e4fcd1c1ecf2
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2018-01-18 21:51:52 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -32,12 +31,11 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
+    const id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.sendStatus(404);
     }
 
-    console.log('sadasas');
     Todo.findById(id).then(todo => {
         if (!todo) {
             return res.sendStatus(404);
@@ -47,6 +45,20 @@ app.get('/todos/:id', (req, res) => {
     }).catch(e => {
         res.sendStatus(400);
     })
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.sendStatus(404);
+    }
+
+    Todo.findByIdAndRemove(id).then(todo => {
+        if (!todo) {
+            return res.sendStatus(404);
+        }
+        res.send(todo);
+    }).catch(() => res.sendStatus(400));
 });
 
 app.listen(port, () => {
