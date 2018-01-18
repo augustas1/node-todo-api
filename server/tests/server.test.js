a2ec92cedf006a038cc9c3b2d0fc4b9bafbacb28
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2018-01-18 09:23:45 +0200

    Test GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-beforeEach(done => {
-     Todo.remove({}).then(() => done());
-});
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
 
 describe('POST /todos', () => {
+
+    beforeEach(done => {
+        Todo.remove({}).then(() => done());
+    });
+
     it('should create a new todo', (done) => {
         var text = 'Test todo text';
 
@@ -51,4 +58,23 @@ describe('POST /todos', () => {
                 }).catch(e => done(e));
             })
     })
+});
+
+describe('GET /todos', () => {
+
+    beforeEach(done => {
+        Todo.remove({}).then(() => {
+            return Todo.insertMany(todos);
+        }).then(() => done());
+    });
+
+    it('should get all todos', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
